3da1a7f883ac7621df255e1aa31e55cb6d006f14
Frank <frank.next@gmx.de>
2018-02-20 05:37:20 +0100

    editable regions now load on demand and unload if distance is getting to big

--- src/js/a-editable/editable-region.js
+++ src/js/a-editable/editable-region.js
@@ -16,7 +16,14 @@ const AFRAME = window.AFRAME;
 const THREE = AFRAME.THREE;
 
 AFRAME.registerComponent('editable-region', {
+  schema: {
+    src: {type: 'string'}
+  },
   init: function () {
+    this.mInitialized = false;
+
+    if (this.data.src) return; // delay rendering content
+
     var el = this.el;
 
     // var element = $(`<a-entity static-body gridhelper="size:50;divisions:50"></a-entity>`);
@@ -56,6 +63,30 @@ AFRAME.registerComponent('editable-region', {
     // $(el).append(element, element2);
 
     $(el).append(playfield);
+  },
+
+  tick: function (time, timeDelta) {
+    // check if actor is close enough or far enough to load unload content
+
+    var actor3D = this.el.sceneEl.camera.parent;
+    var el = this.el;
+
+    var distance = actor3D.position.clone().sub(el.object3D.position).length();
+
+    if (distance > 200 && this.mInitialized == true) {
+      console.log('detach content');
+      this.mInitialized = false;
+
+      $(el).html('');
+    }
+    if (distance < 100 && this.mInitialized == false) {
+      console.log('attach content');
+      this.mInitialized = true;
+      //  var element = $(`<a-entity static-body gridhelper="size:100;divisions:50"></a-entity>`);
+      var element = $(`<a-plane  static-body color="#CCC" rotation="-90 0 0" height="50" width="50"></a-plane>`);
+
+      $(el).append(element);
+    }
   }
 
 });
