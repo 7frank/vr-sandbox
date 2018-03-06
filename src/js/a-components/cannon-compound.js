8caddad7e0b3121d6a40070af48917e0bea54f1c
Frank <frank.next@gmx.de>
2018-03-06 06:12:10 +0100

    made some progress on matching physics with geometry but currently only with globally attached elements

--- src/js/a-components/cannon-compound.js
+++ src/js/a-components/cannon-compound.js
@@ -10,63 +10,6 @@ const THREE = AFRAME.THREE;
  * currently used for more complex gltf objects loaded
  */
 
-/**
- * Creates a wireframe for the body, for debugging.
- * TODO(donmccurdy) – Refactor this into a standalone utility or component.
- * @param  {CANNON.Body} body
- * @param  {CANNON.Shape} shape
- */
-function createWireframe (body, shape) {
-  var offset = shape.offset,
-    orientation = shape.orientation,
-    mesh = CANNON.shape2mesh(body).children[0];
-  var wireframe;
-  wireframe = new THREE.LineSegments(
-    new THREE.EdgesGeometry(mesh.geometry),
-    new THREE.LineBasicMaterial({color: 0xff0000})
-  );
-
-  if (offset) {
-    wireframe.offset = offset.clone();
-  }
-
-  if (orientation) {
-    orientation.inverse(orientation);
-    wireframe.orientation = new THREE.Quaternion(
-      orientation.x,
-      orientation.y,
-      orientation.z,
-      orientation.w
-    );
-  }
-
-  return wireframe;
-}
-
-/**
- * Updates the debugging wireframe's position and rotation.
- */
-function syncWireframe (wireframe) {
-  var offset;
-
-  // Apply rotation. If the shape required custom orientation, also apply
-  // that on the wireframe.
-  wireframe.quaternion.copy(this.body.quaternion);
-  if (wireframe.orientation) {
-    wireframe.quaternion.multiply(wireframe.orientation);
-  }
-
-  // Apply position. If the shape required custom offset, also apply that on
-  // the wireframe.
-  wireframe.position.copy(this.body.position);
-  if (wireframe.offset) {
-    offset = wireframe.offset.clone().applyQuaternion(wireframe.quaternion);
-    wireframe.position.add(offset);
-  }
-
-  wireframe.updateMatrix();
-}
-
 AFRAME.registerComponent('cannon-compound', {
   schema: {
     debug: {type: 'boolean', default: false}
@@ -77,16 +20,9 @@ AFRAME.registerComponent('cannon-compound', {
 
     this.el.addEventListener('model-loaded', function () {
       // the compound generating part
-      testCompoundGLTF(this.el, this.data.debug);
 
-      // a reverse test that  generates wireframes from body instead above one from mesh boundingbox to check for errors of collision
-      this.el.body.shapes.forEach((shape) => {
-        var wireframe = createWireframe(this.el.body, shape);
-        wireframe.name = 'wireframe-bb-physics';
-        syncWireframe.bind(this.el)(wireframe);
-        this.el.sceneEl.object3D.add(wireframe);
-        // this.el.object3D.add(wireframe);
-      });
+      // TODO looks like the matrizes aren't calculated initally which makes sense as the model is only loaded
+      setTimeout(() => testCompoundGLTF(this.el, this.data.debug), 500);
     }.bind(this));
   }
 });
--- src/js/selector-util.js
+++ src/js/selector-util.js
@@ -6,7 +6,10 @@ import * as CANNON from 'cannon';
 import * as _ from 'lodash';
 import {createNamespace, namespaceExists, namespaceInfo} from './namespace';
 
+var mesh2shape = require('three-to-cannon');
+
 const AFRAME = window.AFRAME;
+const THREE = AFRAME.THREE;
 
 AFRAME.nk = {querySelectorAll};
 var debug = false;
@@ -302,8 +305,14 @@ export function testCompoundGLTF (modelEl, debug = false) {
   function localToObject (vector, object3D) {
     return vector.applyMatrix4(object3D.matrix);
   }
-
+  var i = 0;
   items.forEach(el => {
+    if (i++ >= 10) {
+      el.visible = false;
+
+      return;
+    } // TODO only use first few elements for testing
+
     if (el.geometry) el.geometry.computeBoundingBox();
 
     var bb = el.geometry.boundingBox;
@@ -316,9 +325,22 @@ export function testCompoundGLTF (modelEl, debug = false) {
     //  var regionPosition = modelEl.object3D.position.clone().applyMatrix4(region.object3D.matrix);
 
     // var vPosition = regionPosition.add(vCenter);
-    console.log(el.matrixWorld);
+    //  console.log(el.matrixWorld);
     // var vPosition = el.localToWorld(el.position.clone());
-    var vPosition = el.position.clone().applyMatrix4(region.object3D.matrix).add(vCenter);
+    // var vPosition = el.position.clone().applyMatrix4(region.object3D.matrix).add(vCenter);
+
+    // TODO shouldn't the center be applied Oo?
+    var vPosition = el.getWorldPosition();// .applyMatrix4(region.object3D.matrix)
+    // .add(vCenter);
+
+    var vScale = el.getWorldScale();
+    var vQuaternion = el.getWorldQuaternion();
+
+    var testMult = 1;
+    size.x *= vScale.x * testMult;
+    size.y *= vScale.y * testMult;
+    size.z *= vScale.z * testMult;
+
     //    var vPosition = el.position.clone().add(vCenter);
     // var vPosition = el.position.clone().add(vCenter);
 
@@ -326,27 +348,70 @@ export function testCompoundGLTF (modelEl, debug = false) {
     //    vPosition = localToObject(vPosition, modelEl.object3D);
     // vPosition = localToObject(vCenter, modelEl.object3D);
 
+    /* // ---------------------------------------
+
     var shape = new CANNON.Box(new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2));
 
-    modelEl.body.addShape(shape, new CANNON.Vec3(vPosition.x, vPosition.y, vPosition.z));
+    var cannonPos = new CANNON.Vec3(vPosition.x, vPosition.y, vPosition.z);
 
+    modelEl.body.addShape(shape, cannonPos);
+*/
+    // ---------------------------------------
+    /*
+     var boxShape = new CANNON.Box(new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2));
+    var boxBody = new CANNON.Body({ mass: 000 });
+    boxBody.addShape(boxShape);
+    boxBody.position.set(vPosition.x, vPosition.y, vPosition.z);
+    modelEl.body.world.addBody(boxBody);
+*/
+    // ---------------------------------------
+    /* var geometry = new THREE.BoxGeometry(size.x / 2, size.y / 2, size.z / 2);
+    var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.copy(vPosition);
+    modelEl.sceneEl.object3D.add(cube);
+*/
+    // ---------------------------------------
+
+    // ok so far the closest to something working ...
+    // el=jQuery("#physicsTestRegion").find("[gltf-model]")[0]
+    var boxShape = new CANNON.Box(new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2));
+    var boxBody = new CANNON.Body({ mass: 0 });
+    boxBody.addShape(boxShape);
+    boxBody.position.set(vPosition.x, vPosition.y, vPosition.z);
+    boxBody.quaternion.set(vQuaternion.x, vQuaternion.y, vQuaternion.z, vQuaternion.w);
+
+    modelEl.body.world.addBody(boxBody); var geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
+    var material = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.set(vPosition.x, vPosition.y, vPosition.z);
+
+    cube.quaternion.set(vQuaternion.x, vQuaternion.y, vQuaternion.z, vQuaternion.w);
+
+    modelEl.sceneEl.object3D.add(cube);
+
+    // TODO the rotatiom is wrong
+    // el=jQuery("#physicsTestRegion").find("[gltf-model]")[0];el.object3D.quaternion.x=0.7;
+
+    // -------------------------------------------------
     if (debug) {
       // console.log(el);
       // console.log('position', stringifyWithPrecision(vPosition), 'size', stringifyWithPrecision(size));
 
-      console.log('position', stringifyWithPrecision(vPosition));
-
       var helper = addBoundingBox(el);
 
-      modelEl.addEventListener('collide', function (e) {
+      // TODO collision per sub-element not working
+      boxBody.addEventListener('collide', _.debounce(function (e) {
         // console.log(modelEl, 'has collided with body ', e.detail.body);
-        e.detail.body.el.setAttribute('color', 'red');
+        console.log('collision', e);
+        e.body.el.setAttribute('color', _.sample('red', 'yellow', 'blue'));
+      // console.log('collision', e);
         /*   e.detail.target.el;  // Original entity (playerEl).
-                                                                    e.detail.body.el;    // Other entity, which playerEl touched.
-                                                                    e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
-                                                                    e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
-                                                               */
-      });
+            e.detail.body.el;    // Other entity, which playerEl touched.
+            e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
+            e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+       */
+      }, 50));
     }
   });
 
