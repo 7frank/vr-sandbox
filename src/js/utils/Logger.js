a32b7e219c1f1e328c5bad5043feee2c3aa00624
Frank <frank.next@gmx.de>
2018-03-23 23:44:04 +0100

    basic logger similar  to "java.util.logging.Logger"

---
+++ src/js/utils/Logger.js
@@ -0,0 +1,113 @@
+import * as _ from 'lodash';
+
+/**
+ * a handler method for the proxy that caches method calls to window.console
+ * @type {{get: handler.get}}
+ */
+
+var handler = {
+  get: function (target, name) {
+    if (typeof console[name] == 'function') {
+      return function (...args) {
+        target.cached.push({method: name, args});
+      };
+    } else {
+      return target[name];
+    }
+  }
+};
+
+/**
+ * The base factory that's used to create the actual logger.
+ * TODO logs to console if logger is requested a second time
+ */
+
+class _Logger {
+  /**
+     * creates a logger that can be refreences by its name.
+     * @param name
+     * @param options - TODO have some meaningful options
+     * @returns {*}
+     */
+
+  getLogger (name, options) {
+    var proxy = new Proxy({cached: []}, handler);
+
+    if (this[name]) return this[name];
+
+    Reflect.defineProperty(this, name, {
+      get: function () {
+        clearConsole();
+
+        _.each(proxy.cached, function (entry) {
+          var key = entry.method;
+          var args = entry.args;
+          console[key].apply(console, args);
+        });
+
+        return proxy;
+      },
+      set: function (value) {
+        console.warn("can't override");
+      }
+      /* writable: false,
+                        configurable: false,
+                        enumerable: true */
+    });
+
+    return proxy;
+  }
+
+  /**
+     * Setting this to false will stop logging anything. This should prevent heap polution.
+     * Primarily you want to use it in production to disable all logging.
+     *
+     * TODO check that it actually does what it should
+     * @param {boolean} activateLogging
+     */
+
+  /* setState(activateLogging=true){
+    this.bActive=  activateLogging
+    } */
+}
+
+/**
+ * The actual logger that buffers console logs errors warnings ..etc to different channels and keeps the console clean and structured until needed.
+ * Similar to <b>java.util.logging.Logger</b> {@link https://docs.oracle.com/javase/8/docs/api/java/util/logging/Logger.html}
+ *
+ * FIXME sometime doesn't log stuff
+ * TODO have a max life time per log entry to prevent memory growth
+ * TODO have a silent option that not logs at all
+ *
+ * usage:
+ *      (1) create a silent logger
+ *
+ *      var console = Logger.getLogger('sketchfab-browser');
+ *
+ *      (2) log to console
+ *
+ *      Logger['sketchfab-browser']
+ *
+ */
+
+export const Logger = new _Logger();
+window.Logger = Logger;
+
+/**
+ * utility clear console
+ *
+ */
+
+function clearConsole () {
+  var API;
+
+  if (typeof console._commandLineAPI !== 'undefined') {
+    API = console._commandLineAPI; // chrome
+  } else if (typeof console._inspectorCommandLineAPI !== 'undefined') {
+    API = console._inspectorCommandLineAPI; // Safari
+  } else if (typeof console.clear !== 'undefined') {
+    API = console;
+  }
+
+  API.clear();
+}
