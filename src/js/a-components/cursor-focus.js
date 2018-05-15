dc2ba6acfbc7cbb76d39acd62a1c4679bd8f9330
Frank <frank.next@gmx.de>
2018-05-16 00:40:24 +0200

    cleaned up cursor-focus component
    Signed-off-by: Frank <frank.next@gmx.de>

--- src/js/a-components/cursor-focus.js
+++ src/js/a-components/cursor-focus.js
@@ -6,26 +6,24 @@ import {onElementChange} from '../utils/listener-utils';
 import {updateHotComponent} from '../utils/aframe-debug-utils';
 
 /**
- * fixes  keyboard support for non-control elements
+ * Fixes  keyboard support for non-control elements.
+ * Note: very el that the mouse hovers will have this applied once. This way actions can be bound directly to the el instead of using the Hotkeys notation.
+ * TODO this action handling might change in a future version
  * @param target
  */
-function fixFocusAndOtherThingsForNow (target) {
+function fixFocusable (target) {
   if (target.__hasFocusFixed) return;
 
   if (target.hasAttribute && !target.hasAttribute('tabIndex')) {
     target.setAttribute('tabIndex', '-1');
   }
 
-  // TODO handle cases where this is not wanted
-  target.addEventListener('mouseenter', () => target.focus());
-  target.addEventListener('mouseleave', () => target.blur());
-
   target.__hasFocusFixed = true;
 }
 
 /**
  * A helper component that sets focus to the element under the cursor component.
- * This is necessary for keyboard events and hotkeys to be forwarded to the correct element
+ * This is necessary for keyboard events and hotkeys to be forwarded to the correct element.
  *
  */
 
@@ -33,59 +31,28 @@ updateHotComponent('cursor-focus');
 AFRAME.registerComponent('cursor-focus', {
   schema: {},
   init: function () {
+    // keep track of the cursor component
     var initCursor = () => {
       this.mCursor = document.querySelector('[cursor]');
       if (this.mCursor) { this.updateCursorHandlers(); }
     };
 
     initCursor();
-
     onElementChange(undefined, '[cursor]', initCursor);
-
-    // https://github.com/aframevr/aframe/blob/master/docs/components/cursor.md
-    // see states "cursor-hovering"	and "cursor-hovered"
-
-    /*
-
-          // ---------------------------------
-
-          // TODO find or create  change event if another element is intersected in which case the focus will be set to that one
-          var cursor = document.querySelector('[cursor]');
-
-          cursor.addEventListener('change');
-
-          // ---------------------------------
-
-          document.querySelector('[cursor-hovered]');
-
-          // ---------------------------------
-
-          var intersects = document.querySelector('[cursor]').components['cursor'].intersectedEl;
-          var id = appendImageToDOM(url).id;
-
-          console.log('intersects', intersects);
-          console.log('id', id);
-
-          intersects.setAttribute('material', {src: '#' + id});
-
-          // ---------------------------------
-
-          fixFocusAndOtherThingsForNow(intersects.el);
-          intersects.el.focus;
-
-         */
   },
   updateCursorHandlers: function () {
-    console.log('updateCursorHandlers');
     var that = this;
 
     this.mCursor.addEventListener('mouseenter', function (evt) {
       var targetEl = evt.detail.intersectedEl;
-      fixFocusAndOtherThingsForNow(targetEl);
+      // console.log('cursor-focus mouseenter', evt.detail);
+      fixFocusable(targetEl);
+      targetEl.focus();
     });
 
     this.mCursor.addEventListener('mouseleave', function (evt) {
       var targetEl = evt.detail.intersectedEl;
+      targetEl.blur();
     });
   },
 
