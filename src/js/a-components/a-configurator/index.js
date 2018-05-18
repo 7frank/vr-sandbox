45b9e38a0e98f7c8bd380ed0c1087543657ef87a
Frank <frank.next@gmx.de>
2018-05-18 02:42:00 +0200

    sample product configurator for simple-car

--- src/js/a-components/a-configurator/index.js
+++ src/js/a-components/a-configurator/index.js
@@ -1,3 +1,6 @@
+import './gui-list-view';
+import './gui-color-list';
+
 import _ from 'lodash';
 import {appendHTML3D, createHTML} from '../../utils/dom-utils';
 
@@ -40,27 +43,22 @@ AFRAME.registerComponent('product-configurator', {
 
     };
 
-    var blackCar = this.el;// document.querySelectorAll('a-simple-car')[1];
+    var el = this.el;// document.querySelectorAll('a-simple-car')[1];
 
-    blackCar.innerHTML = '';
-
-    var mats = AFRAME.nk.querySelectorAll(blackCar, '.Mesh:where(material)');
+    var mats = AFRAME.nk.querySelectorAll(el, '.Mesh:where(material)');
 
     var [chassis, ...tires] = mats.map(el => el.material);
 
     // load product //no additional meshes for now
 
-    // tires[0] emissiveColor=> rim //aber auch karossierie des autos
-    // tires[1] color=> tread //aber auch frontblende des autos
-
     // ------------------------------
 
     function createColorListView (parent, caption, colorMap, handler, options) {
       var template = `
-                    <a-entity test  scale='.25 .25 .25' >
-                      <a-entity gui-color-list="caption:${caption}"></a-entity>             
-                    </a-entity>
-                    `;
+                            <a-entity class="backPlane"  scale='.25 .25 .25' >
+                              <a-entity gui-color-list="caption:${caption}"></a-entity>             
+                            </a-entity>
+                            `;
 
       var el = createHTML(template);
 
@@ -74,90 +72,23 @@ AFRAME.registerComponent('product-configurator', {
 
     // ------------------------------
 
-    createColorListView(blackCar, 'body reflect', accents, function (e) {
+    createColorListView(el, 'body reflect', accents, function (e) {
       tires.forEach(function (tireMaterials) {
+        // tires[0] emissiveColor=> rim //aber auch karossierie des autos
         tireMaterials[0].emissive = new THREE.Color(e.detail.color);
       });
     }, {position: '4 6 0'});
 
-    createColorListView(blackCar, 'tires', accents, function (e) {
+    createColorListView(el, 'tires', accents, function (e) {
       tires.forEach(function (tireMaterials) {
+        // tires[1] color=> tread //aber auch frontblende des autos
         tireMaterials[1].color = new THREE.Color(e.detail.color);
       });
     }, {position: '-5 6 0'});
-  }
-
-});
-
-/**
- * Simple color select with accent colors.
- * Note: default selection of color is triggered by action: "interaction-pick" not click event
- */
-
-AFRAME.registerComponent('gui-color-list', {
-  schema: {
-    caption: {type: 'string', default: 'color-select'}
   },
-
-  tick: function () {
-    // this.el.object3D.rotation.setFromRotationMatrix(document.querySelector('[camera]').object3D.matrix);
-    this.el.object3D.setRotationFromQuaternion(document.querySelector('[camera]').object3D.quaternion);
-
-    // this.el.object3D.rotation.setFromRotationMatrix(document.querySelector('[camera]').object3D.matrix);
-    // this.el.querySelector('[test]').object3D.setRotationFromQuaternion(document.querySelector('[camera]').object3D.quaternion);
-  },
-  init: function () {
-    var accents = {
-      'Magenta': '255,0,151',
-      'Purple': '162,0,255',
-      'Teal': '0,171,169',
-      'Lime': '140,191,38',
-      'Brown': '160,80,0',
-      'Pink': '230,113,184',
-      'Orange': '240,150,9',
-      'Blue': '27,161,226',
-      'Red': '229,20,0',
-      'Green': '51,153,51'
-
-    };
-
-    var colorButtons = _(accents)
-      .map((v, k) => ` <a-gui-button  width="2" height="0.5" gui-button="backgroundColor:rgb(${v})" value="${k}"></a-gui-button>`)
-      .value()
-      .join('');
-
-    this.el.innerHTML = `
-
-   
-  <a-gui-flex-container 
-        align-items="normal"
-        flex-direction="column"
-        component-padding="0.1"
-        opacity="0"
-        width="2"      
-   
->
-
-            <a-gui-label  width="2" height="0.5" value="${this.data.caption}"></a-gui-label>
-            ${colorButtons}
-
-
-   </a-gui-flex-container>
-                
-
-
-`;
-
-    // -----------------------------------------
-
-    var colorButtons = this.el.querySelectorAll('a-gui-button').toArray();
-
-    colorButtons.forEach((btn) => btn.addEventListener('interaction-pick', e => {
-      var selectedColor = btn.getAttribute('gui-button').backgroundColor;
-
-      e.stopPropagation();
-      this.el.emit('change', {color: selectedColor});
-    }));
+  remove: function () {
+    var planes = this.el.querySelectorAll('.backPlane');
+    planes.forEach(a => a.parentEl.remove(a));
   }
 
 });
