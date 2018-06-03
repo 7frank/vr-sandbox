f761c856a5ed2a08a7d530e3b03b398beaa3fc02
Frank <frank.next@gmx.de>
2018-06-04 00:54:28 +0200

    improved procedural-terrian - supporting texture now - player may walk on geometry (y-position is set to heightmap y-position)

--- src/js/a-components/procedural-terrain.js
+++ src/js/a-components/procedural-terrain.js
@@ -1,6 +1,9 @@
-
-import {createTerrain} from '../utils/terrain-utils';
+import {createTerrain, generateBlendedMaterial} from '../utils/terrain-utils';
 import {updateHotComponent} from '../utils/aframe-debug-utils';
+import {getWorldDirection, getWorldPosition, getWorldQuaternion, restartPhysics, toast} from '../utils/aframe-utils';
+import {getPlayer} from '../game-utils';
+import * as _ from 'lodash';
+import {Box3Ext} from '../three/Box3Ext';
 
 /**
  *  A terrain generator
@@ -8,17 +11,90 @@ import {updateHotComponent} from '../utils/aframe-debug-utils';
  */
 updateHotComponent('procedural-terrain');
 AFRAME.registerComponent('procedural-terrain', {
-  schema: {
-
-  },
+  schema: {},
   init: function () {
     console.log('procedural-terrain');
 
-    var mesh = createTerrain();
-    this.el.object3D.add(mesh);
+    let sampleMaterial = this.createSampleMaterial();
+    // ------------------------
+
+    var mesh = createTerrain(64, 64, -50, 50, sampleMaterial);
+
+    this.el.setObject3D('mesh', mesh, mesh.data);
+
+    // create a aabb boundingbox in world coordinates for position testing
+    var box = new Box3Ext();
+    box.setFromObject(this.el.object3D, null, true, true);
+    this.bb = box;
+  },
+  createSampleMaterial: function () {
+    // ------------------------
+    // query the dom for some materials
+    var textures0 = AFRAME.nk.querySelectorAll(this.el.sceneEl, ':where(material-map-image)').map(m => m.material.clone().map);
+    var textures = _.uniq(textures0, 'image');
+    var [x, t1, t2, t3, t4] = textures;
+
+    var material = generateBlendedMaterial([
+      // The first texture is the base; other textures are blended in on top.
+      {texture: t1},
+      // Start blending in at height -80; opaque between -35 and 20; blend out by 50
+      {texture: t2, levels: [-40, -17, 10, 25]},
+      {texture: t3, levels: [10, 25, 30, 42]},
+      // How quickly this texture is blended in depends on its x-position.
+      {
+        texture: t4,
+        glsl: '1.0 - smoothstep(65.0 + smoothstep(-256.0, 256.0, vPosition.x) * 10.0, 80.0, vPosition.z)'
+      },
+      // Use this texture if the slope is between 27 and 45 degrees
+      {
+        texture: t3,
+        glsl: 'slope > 0.7853981633974483 ? 0.2 : 1.0 - smoothstep(0.47123889803846897, 0.7853981633974483, slope) + 0.2'
+      }
+    ]);
+
+    return material;
   },
   tick: function (time, timeDelta) {
+    this.putPlayerOnHeightMap();
+  },
+  putPlayerOnHeightMap: function () {
+    let hm = this.el.getObject3D('mesh').data;
+    // let posEl = getWorldPosition(this.el.object3D);
 
+    let pos = getWorldPosition(getPlayer().object3D);
+
+    // pos.add(posEl.clone().sub(pos));
+
+    // let pos = new THREE.Vector3(playerPos.x, playerPos.y, playerPos.z);
+
+    let pctX = (pos.x - this.bb.min.x) / (this.bb.max.x - this.bb.min.x);
+    let pctY = (pos.z - this.bb.min.z) / (this.bb.max.z - this.bb.min.z);
+    let px = pctX * hm.options.xSegments;
+    let py = pctY * hm.options.ySegments;
+
+    if (this.bb.min.x < pos.x && this.bb.max.x > pos.x && this.bb.min.z < pos.z && this.bb.max.z > pos.z) {
+      // interpolate x and y positions
+
+      var direction = getWorldDirection(getPlayer().object3D);
+
+      let baseY = _.get(hm, `heightmap[${parseInt(px)}][${parseInt(py)}]`, 0);
+      let baseY_x = _.get(hm, `heightmap[${parseInt(px + direction.x)}][${parseInt(py)}]`, 0);
+      let baseY_y = _.get(hm, `heightmap[${parseInt(px)}][${parseInt(py + direction.y)}]`, 0);
+
+      let dx = (baseY_x - baseY) * (px % 1);
+      let dy = (baseY_y - baseY) * (py % 1);
+      let y = baseY + (dx + dy) / 2;
+
+      // scale by mesh size
+      y *= (this.bb.max.y - this.bb.min.y) / (hm.options.maxHeight - hm.options.minHeight);
+      // add average player eye height/size
+      y += 1.6;
+
+      // FIXME add offset of this.el to calculations
+      y += 10;
+
+      getPlayer().object3D.position.y = y;
+    }
   }
 
 });
