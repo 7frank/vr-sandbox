97d09e146a575f05ad0fe5eb99eee5389bb79ecc
Frank <frank.next@gmx.de>
2018-06-18 23:31:07 +0200

    AnimationFactory to handle tweens a bit more easily

--- src/js/utils/animation-utils.js
+++ src/js/utils/animation-utils.js
@@ -4,6 +4,16 @@
  * @param el
  * @returns {String[]}
  */
+/**
+ *
+ *
+ * animate(options:Object, mDuration : Number, onComplete:function)
+ *
+ * options does contain all the animated properties
+ */
+
+import TWEEN from '@tweenjs/tween.js';
+
 export function getAnimationNames (el) {
   var component = el.components['animation-mixer'];
   if (!component) {
@@ -40,11 +50,126 @@ export function playAnimation (el, animationName, tCrossfade) {
   component.playAction();
 
   /*
-           var a = component.activeActions[0];
+               var a = component.activeActions[0];
 
-            var b = _.sample(component.mixer._actions);
+                var b = _.sample(component.mixer._actions);
 
-            if (a) { a.crossFadeTo(b, tCrossfade); } else { b.fadeIn(tCrossfade); }
+                if (a) { a.crossFadeTo(b, tCrossfade); } else { b.fadeIn(tCrossfade); }
 
-            */
+                */
+}
+
+/**
+ *
+ * @param origObject
+ * @returns {{animate: (function(this:AnimationFactory)), stop: stop}}
+ * @constructor
+ */
+export function AnimationFactory (origObject) {
+  if (typeof origObject == 'undefined') throw new Error('must be an object');
+
+  // https://github.com/tweenjs/tween.js/issues/78
+  // Flatten/Deflate an object
+  var flatten = function (source, pathArray, result) {
+    pathArray = (typeof pathArray === 'undefined') ? [] : pathArray;
+    result = (typeof result === 'undefined') ? {} : result;
+    var key, value, newKey;
+    for (var i in source) {
+      if (source.hasOwnProperty(i)) {
+        key = i;
+        value = source[i];
+        pathArray.push(key);
+
+        if (typeof value === 'object' && value !== null) {
+          result = flatten(value, pathArray, result);
+        } else {
+          newKey = pathArray.join('.');
+          result[newKey] = value;
+        }
+        pathArray.pop();
+      }
+    }
+    return result;
+  };
+
+  function ref (obj, str) {
+    return str.split('.').reduce(function (o, x) {
+      return o[x];
+    }, obj);
+  }
+
+  // Move values from a flatten object to its original object
+  function returnValue (obj, flattened, key) {
+    let parts = key.split(/\.(?=[^.]+$)/); // Split "foo.bar.baz" into ["foo.bar", "baz"]
+    if (parts.length == 1) {
+      obj[parts[0]] = flattened[key];
+    } else {
+      ref(obj, parts[0])[parts[1]] = flattened[key];
+    }
+  }
+
+  function getValue (obj, key) {
+    let parts = key.split(/\.(?=[^.]+$)/); // Split "foo.bar.baz" into ["foo.bar", "baz"]
+    if (parts.length == 1) {
+      return obj[parts[0]];
+    } else {
+      return ref(obj, parts[0])[parts[1]];
+    }
+  }
+
+  var tween;
+  return {
+    animate: function (options = {}, mDuration = 400, onComplete, onUpdate) {
+      if (tween) tween.stop();
+
+      var mTimeout;
+      var that = origObject;// this;
+      var stopped = false;
+
+      var flattened_to = flatten(options);
+
+      var keys = Object.keys(flattened_to);
+      var flattened_from = {};
+      keys.forEach(k => flattened_from[k] = getValue(that, k));
+
+      // whenever animation is completed or stopped
+      function onFinish () {
+        cancelAnimationFrame(mTimeout);
+
+        stopped = true;
+        if (typeof onComplete == 'function') {
+          onComplete.bind(origObject)();
+        }
+      }
+
+      tween = new TWEEN.Tween(flattened_from);
+      tween.to(flattened_to, mDuration)
+        .onUpdate(function (values) {
+          // Move the values from the flattened and tweening object
+          // to the original object
+
+          for (let key in values) {
+            returnValue(that, values, key);
+          }
+          if (typeof onUpdate == 'function') onUpdate.bind(that)();
+        })
+        .onComplete(onFinish).onStop(onFinish)
+        .start();
+
+      mTimeout = requestAnimationFrame(animate);
+
+      function animate (time) {
+        if (stopped) return;
+
+        tween.update(time);
+        mTimeout = requestAnimationFrame(animate);
+      }
+
+      return this;
+    },
+    stop: function () {
+      tween.stop();
+    }
+
+  };
 }
