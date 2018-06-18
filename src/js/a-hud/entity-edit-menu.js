bee57823fee4af4ab072b3f88e84b9f5725b8892
Frank <frank.next@gmx.de>
2018-06-18 17:57:56 +0200

    + previous commit

--- src/js/a-hud/entity-edit-menu.js
+++ src/js/a-hud/entity-edit-menu.js
@@ -5,6 +5,8 @@ import {showMenu} from '../utils/debug-gui';
 import * as _ from 'lodash';
 
 import TWEEN from '@tweenjs/tween.js';
+import {querySelectorAll} from '../utils/selector-utils';
+import {getWorldDirection, getWorldPosition} from '../utils/aframe-utils';
 
 AFRAME.registerComponent('entity-edit-menu', {
   schema: {},
@@ -40,9 +42,16 @@ AFRAME.registerComponent('entity-edit-menu', {
       return obj;
     }, {});
 
-    function zoomToPos () {
-      var position = { x: _.random(1, 3), y: _.random(1, 3), z: _.random(1, 3) };
-      console.error('TODO zoom', position);
+    function zoomToPos (targetCamera, meshDetails) {
+      var position = new THREE.Vector3(_.random(1, 20) / 10, _.random(1, 20) / 10, _.random(1, 20) / 10);
+      window.ooo = meshDetails;
+      console.error('mesh', meshDetails);
+      // var position = meshDetails.relativePosition.clone();
+      let dir = getWorldDirection(meshDetails.mesh);
+
+      position.add(dir);
+
+      console.error('zoomTo', position);
 
       var tween;
       // Setup the animation loop.
@@ -52,30 +61,48 @@ AFRAME.registerComponent('entity-edit-menu', {
       }
       requestAnimationFrame(animate);
 
-      let targetCamera = refs.sphere.getObject3D('touch-rotate-controls-camera');
-
       console.log('targetCamera', targetCamera);
       tween = new TWEEN.Tween(targetCamera.position) // Create a new tween that modifies 'coords'.
         .to(position, 1000) // Move to (300, 200) in 1 second.
         .easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
         .onUpdate(function () { // Called after tween.js updates 'coords'.
           // Move 'box' to the position described by 'coords' with a CSS translation.
-
         })
         .start(); // Start the tween immediately.
     }
 
-    refs.sphere.addEventListener('click', () => {
+    /*  refs.sphere.addEventListener('click', () => {
       zoomToPos();
-    });
-      TODO use ZoomHelper(other repo)
-      //TODO use ZoomHelper(other repo) for better usability and performance
-    // FIXME it should be possible to get the result of a selction of the llist view this way
+    }); */
+
+    function getMesh (o) {
+      let els = document.querySelectorAll(o.selector).toArray();
+      console.log(o);
+      if (o.part == undefined) o.part = '.Mesh';
+
+      console.log(o.selector, '---', o.part);
+
+      let subQueryResult = querySelectorAll(els[0], o.part);
+
+      // --------
+
+      console.log('getMesh positions', getWorldPosition(els[0].object3D), getWorldPosition(subQueryResult[0]));
+      let relativePosition = getWorldPosition(els[0].object3D).sub(getWorldPosition(subQueryResult[0]));
+      // -----
+
+      return {mesh: subQueryResult[0], relativePosition};
+    }
+
+    // TODO use ZoomHelper(other repo) for better usability and performance
+    // TODO it should be possible to get the result of a selection of the list view this wa
+    // list view component should probably have some default emtters for change and selected
     // selected is not emitted the same way change is (change is working)
     window.oooo = refs;
-    refs.listview.addEventListener('change', ({detail}) => {
-      zoomToPos();
-      console.error(detail);
+    refs.listview.addEventListener('selected', ({detail}) => {
+      let meshDetails = getMesh(detail);
+
+      let targetCamera = refs.sphere.getObject3D('touch-rotate-controls-camera');
+      zoomToPos(targetCamera, meshDetails);
     });
   }
 
